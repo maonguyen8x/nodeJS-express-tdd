9dc39b7923163f428ea4372bf61cb78d75585187
maonguyen8x <nguyenmao.2912@gmail.com>
2022-04-07 13:58:17 +0700

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var todosRouter = require('./routes/todos');
+var todosRouter = require('./routes/todos'); 
 
 var app = express();
 
